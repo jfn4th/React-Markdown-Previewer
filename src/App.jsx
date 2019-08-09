fcabc853d8ae13fb2d02fd5ab0099477738ad9b8
Jamie Nelson <jfn4th@gmail.com>
2019-08-09 15:21:57 -0500

    add marked, remove unneeded files

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,49 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import marked from 'marked';
 
-function App() {
-	return (
-		<div className="App">
-			<header className="App-header">
-				<div />
-			</header>
-		</div>
-	);
+class App extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			input: '',
+			markup: ''
+		};
+		this.handleInput = this.handleInput.bind(this);
+		this.renderMarkup = this.renderMarkup.bind(this);
+	}
+
+	handleInput(event) {
+		this.setState({ input: event.target.value });
+		this.renderMarkup();
+	}
+
+	renderMarkup() {
+		this.setState({ markup: marked(this.state.input) });
+	}
+
+	render() {
+		const markup = this.state.markup;
+		return (
+			<div className="App">
+				<header className="App-header">
+					<div id="editor-area">
+						<textarea
+							name=""
+							id="editor"
+							cols="30"
+							rows="10"
+							onChange={this.handleInput}
+							value={this.state.input}
+						/>
+					</div>
+					<div id="preview-area">
+						<div id="preview">{markup}</div>
+					</div>
+				</header>
+			</div>
+		);
+	}
 }
 
 export default App;
